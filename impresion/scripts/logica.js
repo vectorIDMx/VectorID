35fdd513936366f5e4dd5ff860174251f1583f8e
Ricardohp22 <45410177+Ricardohp22@users.noreply.github.com>
2019-03-04 17:43:01 -0600

    HOLA

--- impresion/scripts/logica.js
+++ impresion/scripts/logica.js
@@ -3,7 +3,7 @@ var botonW = document.getElementById("botonWaId");
 
 botonW.addEventListener("click", function(e)
 {
-    window.open("https://api.whatsapp.com/send?phone=+5215584151591&text=Hola!%20quiero%20saber%20más%20sobre%20el%20marketing%20con%20Realidad%20Aumentada");
+    window.open("https://api.whatsapp.com/send?phone=+5215584151591&text=Hola!!%20quiero%20saber%20más%20sobre%20el%20marketing%20con%20Realidad%20Aumentada");
 });
 botonV.addEventListener("click", function(e)
 {
