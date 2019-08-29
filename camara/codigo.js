767b43a897ec5a84f50aa4bd74d135cc243e8c0d
Ricardohp22 <45410177+Ricardohp22@users.noreply.github.com>
2019-08-29 12:51:05 -0500

    camara start

---
+++ camara/codigo.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const snap = document.getElementById("snap");
+const errorMsgElement = document.querySelector('span#errorMsg');
+
+
+const constraints = {
+  audio: false,
+  video: {
+    width: 1280, height: 720
+  }
+};
+
+// Access webcam
+async function init() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    handleSuccess(stream);
+  } catch (e) {
+    errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
+  }
+}
+
+// Success
+function handleSuccess(stream) {
+  window.stream = stream;
+  video.srcObject = stream;
+}
+
+// Load init
+init();
+
+// Draw image
+/* var context = canvas.getContext('2d');
+snap.addEventListener("click", function() {
+        context.drawImage(video, 0, 0, 640, 480);
+}); */
+
+/* if (typeof MediaStreamTrack === 'undefined' ||
+    typeof MediaStreamTrack.getSources === 'undefined') {
+  alert('This browser does not support MediaStreamTrack.getSources().');
+} else {
+  navigator.mediaDevices.enumerateDevices().then(function(e) {
+    console.log(e);
+  });
+}
+ */
+navigator.mediaDevices.enumerateDevices().then(function(e) {
+  e.forEach(el => {
+    if(el.kind == 'videoinput'){
+      console.log('camara:');
+      console.log(el.label);
+    }
+  });
+  let camaras = e.filter(el => {
+    if(el.kind == 'videoinput'){
+      return el;
+    }
+  });
+  console.log(camaras);
+  
+});
