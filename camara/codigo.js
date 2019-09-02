d51c3f9606f112bce3101cefe44b94781e70ca12
Ricardohp22 <45410177+Ricardohp22@users.noreply.github.com>
2019-09-01 22:50:58 -0500

    dvd

--- camara/codigo.js
+++ camara/codigo.js
@@ -23,7 +23,7 @@ const constraints = {
 //////////////////////
 async function init() {
   
-  console.log("id de la fuentettt: " + constraints.video.optional[0].sourceId);
+  console.log("id de la fuentey: " + constraints.video.optional[0].sourceId);
   try {
     const stream = await navigator.mediaDevices.getUserMedia(constraints);
     handleSuccess(stream);
