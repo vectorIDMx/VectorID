340f78df9c280fe6a5a980c7d2bcd277f0ea4201
Ricardohp22 <45410177+Ricardohp22@users.noreply.github.com>
2019-09-02 02:28:00 -0500

    deadc

---
+++ disparosv2/codigo.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const snap = document.getElementById("snap");
+const errorMsgElement = document.querySelector('span#errorMsg');
+let dispositivo;
+
+const constraints = {
+    audio: false,
+    mandatory: {
+      "minWidth": 1280,
+      "minHeight": 720
+    },
+    video: {
+      optional: [
+        {
+          sourceId: dispositivo
+        }
+      ]
+    }
+  };
+//////////////////////
+async function init() {
+  
+  console.log("id de la fuentey: " + constraints.video.optional[0].sourceId);
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    handleSuccess(stream);
+  } catch (e) {
+    errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
+  }
+}
+//////////////////////
+navigator.mediaDevices.enumerateDevices().then(function(e) {
+  e.forEach(el => {
+    if(el.kind == 'videoinput'){
+      console.log('index: ' + el.label.indexOf('back'));
+      if(el.label.indexOf('back') > 0)
+      {
+        constraints.video.optional[0].sourceId = el.deviceId;
+        console.log('camara:');
+        console.log('dispositivo: ' + constraints.video.optional[0].sourceId);
+      }
+    }
+  })  
+}).then(
+  function(e)
+  {
+    init();
+  });
+//////////////////////
+
+
+
+
+// Access webcam
+
+
+// Success
+function handleSuccess(stream) {
+  window.stream = stream;
+  video.srcObject = stream;
+}
+
+// Load init
+
+/* window.setTimeout(function()
+{
+  console.log("id de la fuente: " + constraints.video.optional[0].sourceId);
+  
+}, 2000); */
+
+
