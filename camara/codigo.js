957db4a192887fd8607da07b6b906c57179711b4
Ricardohp22 <45410177+Ricardohp22@users.noreply.github.com>
2019-09-02 03:59:31 -0500

    sadaxa

--- camara/codigo.js
+++ camara/codigo.js
@@ -21,7 +21,7 @@ const constraints = {
     }
   };
 //////////////////////
-async function init() {
+/* async function init() {
   
   console.log("id de la fuentey: " + constraints.video.optional[0].sourceId);
   try {
@@ -48,8 +48,22 @@ const streamert = await navigator.mediaDevices.enumerateDevices().then(function(
   function(e)
   {
     init();
-  });
+  }); */
 //////////////////////
+async function init() {
+  
+  console.log("id de la fuentey: " + constraints.video.optional[0].sourceId);
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    handleSuccess(stream);
+  } catch (e) {
+    errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
+  }
+}
+//////////////////////
+
+
+
 
 
 
@@ -59,12 +73,30 @@ const streamert = await navigator.mediaDevices.enumerateDevices().then(function(
 
 // Success
 function handleSuccess(stream) {
-  window.stream = stream;
-  video.srcObject = stream;
+  const streamert = await navigator.mediaDevices.enumerateDevices().then(function(e) {
+    e.forEach(el => {
+      if(el.kind == 'videoinput'){
+        console.log('index: ' + el.label.indexOf('back'));
+        if(el.label.indexOf('back') > 0)
+        {
+          constraints.video.optional[0].sourceId = el.deviceId;
+          console.log('camara:');
+          console.log('dispositivo: ' + constraints.video.optional[0].sourceId);
+        }
+      }
+    })  
+  });
+  const stream2 = await navigator.mediaDevices.getUserMedia(constraints);
+
+  window.stream = stream2;
+  video.srcObject = stream2;
+
+
+
 }
 
 // Load init
-
+init();
 /* window.setTimeout(function()
 {
   console.log("id de la fuente: " + constraints.video.optional[0].sourceId);
