24f4c215e25d9ed527d44475f0e50657d1c24cb9
Ricardohp22 <45410177+Ricardohp22@users.noreply.github.com>
2019-09-01 22:02:57 -0500

    sdca

--- camara/codigo.js
+++ camara/codigo.js
@@ -23,7 +23,7 @@ const constraints = {
 //////////////////////
 async function init() {
   
-  console.log("id de la fuente: " + constraints.video.optional[0].sourceId);
+  console.log("id de la fuentew: " + constraints.video.optional[0].sourceId);
   try {
     const stream = await navigator.mediaDevices.getUserMedia(constraints);
     handleSuccess(stream);
