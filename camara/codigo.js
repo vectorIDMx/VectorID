1d147add083602f7252e4759b2180a1863f5bc3e
Ricardohp22 <45410177+Ricardohp22@users.noreply.github.com>
2019-09-02 11:00:01 -0500

    dssv

--- camara/codigo.js
+++ camara/codigo.js
@@ -61,25 +61,7 @@ async function init() {
   }
 }
 //////////////////////
-console.log("si actualizo fredy");
 
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
-
-if (navigator.getUserMedia) {
-   navigator.getUserMedia({audio: true, video: true}, function(stream) {
-    if (navigator.webkitGetUserMedia) {
-      //video.src = window.webkitURL.createObjectURL(stream);
-      console.log("juyt");
-    } else {
-      //video.src = stream; //Opera
-      console.log("loik");
-    }
-  }, onFailSoHard); 
-  console.log("si entro");
-} else {
-  //video.src = 'somevideo.webm'; //fallback.
-  console.log("no entro");
-}
 
 /* const streamert = await navigator.mediaDevices.enumerateDevices().then(function(e) {
     e.forEach(el => {
@@ -102,14 +84,42 @@ if (navigator.getUserMedia) {
 function handleSuccess(stream) {
   window.stream = stream;
   video.srcObject = stream;
+  console.log('Abre camara con exito');
+  navigator.mediaDevices.enumerateDevices().then(function (e) {
+    e.forEach(el => {
+      if (el.kind == 'videoinput') {
+        console.log('id: '+el.deviceId);
+        console.log('index: ' + el.label.indexOf('back'));
+        if (el.label.indexOf('back') > 0) {
+          constraints.video.optional[0].sourceId = el.deviceId;
+          console.log('camara:');
+          console.log('dispositivo: ' + constraints.video.optional[0].sourceId);
+        }
+      }
+    })
+  }).then(
+    navigator.mediaDevices.getUserMedia(constraints)
+  );
+  
 }
 
 // Load init
-//init();
+async function init() {
+
+  console.log("id de la fuentey: " + constraints.video.optional[0].sourceId);
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    handleSuccess(stream);
+  } catch (e) {
+    errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
+  }
+}
+//////////////////////
+
+
+init();
 /* window.setTimeout(function()
 {
   console.log("id de la fuente: " + constraints.video.optional[0].sourceId);
 
 }, 2000); */
-
-
