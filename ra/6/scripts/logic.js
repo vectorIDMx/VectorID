f308f8c235ba073c6d7defc1badb34a01dcb609b
Ricardohp22 <45410177+Ricardohp22@users.noreply.github.com>
2019-09-04 17:55:36 -0500

    sxsa

--- ra/6/scripts/logic.js
+++ ra/6/scripts/logic.js
@@ -1,6 +1,6 @@
 console.log("Logic.js is working!");
 document.getElementById('red1').addEventListener('click', function(e){
-    window.open('https://api.whatsapp.com/send?phone=+5215544602389&text=Hola!%20en%20que%20podemos%20ayudarte?');
+    window.open('https://www.vectorid.mx/ra/6/agendar-contacto.vcf');
 })
 document.getElementById('red2').addEventListener('click', function(e){
     window.open('tel:6441560226');
@@ -9,7 +9,7 @@ document.getElementById('red3').addEventListener('click', function(e){
     window.open('https://www.vectorid.mx/');
 })
 document.getElementById('red4').addEventListener('click', function(e){
-    window.open('https://api.whatsapp.com/send?phone=+5215544602389&text=Hola!%20en%20que%20podemos%20ayudarte?');
+    window.open('https://api.whatsapp.com/send?phone=+51930484626&text=Hola!%20somos%20Kapta%20studio,%20en%20que%20podemos%20ayudarte?');
 })
 document.getElementById('red5').addEventListener('click', function(e){
     window.open('fb://page/299366750897285');
@@ -18,5 +18,5 @@ document.getElementById('red6').addEventListener('click', function(e){
     window.open('https://www.instagram.com/kaptastudio/');
 })
 document.getElementById('red7').addEventListener('click', function(e){
-    window.open('https://www.skype.com/es/');
+    window.open('skype:++51930484626?call');
 })
