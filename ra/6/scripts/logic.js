ca5ec5dc2ad395e96578febb525093e6291afc19
Ricardohp22 <45410177+Ricardohp22@users.noreply.github.com>
2019-09-07 12:12:18 -0500

    xa<xsa

--- ra/6/scripts/logic.js
+++ ra/6/scripts/logic.js
@@ -22,23 +22,30 @@ document.getElementById('red7').addEventListener('click', function(e){
 }) */
 
 document.getElementById('cilbox1').addEventListener('click', function(e){
-    alert("click al plano1");
+    //alert("click al plano1");
+    window.open('https://www.vectorid.mx/ra/6/agendar-contacto.vcf');
 })
 document.getElementById('pred2').addEventListener('click', function(e){
-    alert("click al plano2");
+    //alert("click al plano2");
+    window.open('tel:51930484626');
 })
 document.getElementById('pred3').addEventListener('click', function(e){
-    alert("click al plano3");
+    alert("Web Page pendiente");
+    //window.open('https://www.vectorid.mx/');
 })
 document.getElementById('pred4').addEventListener('click', function(e){
-    alert("click al plano4");
+    //alert("click al plano4");
+    window.open('https://api.whatsapp.com/send?phone=+51930484626&text=Hola!%20somos%20Kapta%20studio,%20en%20que%20podemos%20ayudarte?');
 })
 document.getElementById('pred5').addEventListener('click', function(e){
-    alert("click al plano5");
+    alert("pagina Facebook pendiente");
+    //window.open('fb://page/299366750897285');
 })
 document.getElementById('pred6').addEventListener('click', function(e){
-    alert("click al plano6");
+    //alert("click al plano6");
+    window.open('https://www.instagram.com/kaptastudio/');
 })
 document.getElementById('pred7').addEventListener('click', function(e){
-    alert("click al plano7");
+    //alert("click al plano7");
+    window.open('skype:+51930484626?call');
 })
